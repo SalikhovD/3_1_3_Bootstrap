3c64e0d173b24197f4a02f8bb27cf98fb5443257
SalikhovD <salikhvdima@gmail.com>
2022-10-05 19:41:05 +0300

    Исправление замечаний

--- src/main/java/ru/kata/spring/boot_security/demo/entity/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/entity/User.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.Collection;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -123,5 +124,18 @@ public class User implements UserDetails {
                 ", age=" + age +
                 ", roles=" + roles +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return username.equals(user.username) && password.equals(user.password) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(age, user.age) && Objects.equals(roles, user.roles);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password, firstName, lastName, age, roles);
     }
 }
